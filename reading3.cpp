d0fc2de2cfefc05c09d3ea20110a891ab94f878b
David Liang <davidxliang@gmail.com>
2017-04-01 22:04:05 -0700

    finished making it work with 2L 2R 3R and X

--- reading3.cpp
+++ reading3.cpp
@@ -76,15 +76,10 @@ void algorith_search(vector< vector<location> > &, location, bool, int) ; //this
 void printCluster(vector< vector<location> > &, ofstream& ) ; //this prints the cluster to the file 
 void search_potential_breaks(vector< vector<location> > &, vector< vector<location> > &, int, ofstream&, ofstream&) ; //this will use both forward and reverse clusters and try to find a potential breakpoint  
 bool sortByLocation(const location &lhs , const location &rhs) ; //this sorts by position
-bool compByLocation(const location &lhs , const location &rhs) ; //this compares and give me the unique location 
 vector<location> duplicate( vector<location> &, vector<location> & ) ; 
 void merging_clusters( vector< vector<location> > &) ; //this is just condensing the clusters into one big cluster to avoid duplications in the goodFile
-vector<location> intersection(vector<location>  &, vector<location> &) ; 
-vector<location> unique_difference(vector<location> & , vector<location> &) ; 
-bool compByLocation1(const location &lhs , const location &rhs) ; //this compares and give me the unique location 
-                //test_2 = unique_difference(merged[0], current[location_counter]) ; 
 //test running program on 303_parsed.sam
-bool compFirst(vector<location>, vector<location>) ; 
+ 
 int main(int argc, char * argv[]){
 
     int cluster_size = 3000  ; 
@@ -120,10 +115,17 @@ int main(int argc, char * argv[]){
     }
     string line;
     vector< vector<location> > for_clusters ; //default here just to be safe for the program to work
+    vector< vector<location> > for_clusters_1 ;
+    vector< vector<location> > for_clusters_2 ;
+    vector< vector<location> > for_clusters_3 ;
     vector< vector<location> > rev_clusters ; 
+    vector< vector<location> > rev_clusters_1 ; 
+    vector< vector<location> > rev_clusters_2 ; 
+    vector< vector<location> > rev_clusters_3 ; 
     vector< vector<location> > potential ; 
+
     while(getline(file, line)){ // \n is the default delimiter here, so this goes down the input
-        
+        int counter = 0 ; 
         stringstream ss(line);
         location data1;
         ss >> data1;
@@ -143,12 +145,12 @@ int main(int argc, char * argv[]){
             continue; //skipping the ones that aren't sutiable candidates'
         }
         // cout << data1.samFlag << " this is a test" << endl ; //everything is being read fine
-        if( (data1.chrom_pos == chrom_region_2) || (data1.chrom_pos == chrom_region_1) || (data1.chrom_pos == chrom_region_2) || (data1.chrom_pos == chrom_region_3) ){
-
-            //don't remove the comment lines until you have the merging part done with I22 or 319 whatever to make it work. '
-            //do nothing here since I'm looking for these areas of the chromosomes
-
+        if(data1.chrom_pos == chrom_region){ /*cout << " data " << endl ;*/ counter = 0;} 
+        else if (data1.chrom_pos == chrom_region_1){ /*cout << " data 1 " << endl ;*/ counter = 1;} 
+        else if (data1.chrom_pos == chrom_region_2){ /*cout << " data 2 " << endl ;*/ counter = 2;}  
+        else if (data1.chrom_pos == chrom_region_3){ /*cout << " data 3 " << endl ;*/ counter = 3; 
         }else{
+            // cout << " this is the chrom_region " << data1.chrom_pos << " should it be here? " << endl ; 
             garbageFile << data1.ID << "\t" << data1.samFlag << "\t" << data1.chrom_pos << "\t" << data1.map_quality << \
             "\t" << data1.position1 << "\t" << data1.position2 << "\n" ;             
             continue ; 
@@ -156,14 +158,12 @@ int main(int argc, char * argv[]){
         //remember to add on the secondary alignments once you're done with the core purpose'
         //checking for reverse if it's there and if there are forward strands then continue leaving no other strands.
         if ( (data1.samFlag == rev_1 || data1.samFlag == rev_2 || data1.samFlag == sec_rev_1 || data1.samFlag == sec_rev_2) ) { 
-            //this works
             data1.reverse = true ;
             data1.forward = false ;  
         }else if( (data1.samFlag == for_1 || data1.samFlag == for_2 || data1.samFlag == sec_for_1 || data1.samFlag == sec_for_2) ){
             data1.reverse = false ;
             data1.forward = true ; 
-        }
-        else if ( (data1.samFlag != for_1 || data1.samFlag != for_2 || data1.samFlag != sec_for_1 || data1.samFlag != sec_for_2) \
+        } else if ( (data1.samFlag != for_1 || data1.samFlag != for_2 || data1.samFlag != sec_for_1 || data1.samFlag != sec_for_2) \
          &&  (data1.samFlag != rev_1 || data1.samFlag != rev_2 || data1.samFlag != sec_rev_1 || data1.samFlag != sec_rev_2) ) {        
             continue ; 
         }
@@ -171,10 +171,26 @@ int main(int argc, char * argv[]){
         //check why the forward clusters has a shit ton
         bool in_cluster = false ; 
         if( (data1.reverse == true) && (data1.forward != true) ) { 
-            algorith_search(rev_clusters, data1, in_cluster,cluster_size) ;
+            if(counter == 0){ 
+                algorith_search(rev_clusters, data1, in_cluster,cluster_size) ;
+            }else if (counter == 1){
+                algorith_search(rev_clusters_1, data1, in_cluster,cluster_size) ;
+            }else if (counter == 2){
+                algorith_search(rev_clusters_2, data1, in_cluster,cluster_size) ;
+            }else if (counter == 3){
+                algorith_search(rev_clusters_3, data1, in_cluster,cluster_size) ;
+            }
         }else if( (data1.reverse != true) && (data1.forward == true) ){ 
-            algorith_search(for_clusters, data1, in_cluster,cluster_size) ;
+            if(counter == 0){  
+                algorith_search(for_clusters, data1, in_cluster,cluster_size) ;
             //printCluster(for_clusters, garbageFile) ; 
+            }else if(counter == 1){ 
+                algorith_search(for_clusters_1, data1, in_cluster,cluster_size) ;
+            }else if( counter == 2){ 
+                algorith_search(for_clusters_2, data1, in_cluster,cluster_size) ;
+            }else if( counter == 3) {
+                algorith_search(for_clusters_3, data1, in_cluster,cluster_size) ;
+            }
         }
 
         //creating a vector that holds all of the forward first entry position to be tested with the reverse clusters later on.
@@ -182,11 +198,25 @@ int main(int argc, char * argv[]){
             vector<location> new_clusters ; 
             new_clusters.push_back( data1 ) ; 
             if(data1.reverse){
-                rev_clusters.push_back( new_clusters ) ; 
+                if(counter == 0){ 
+                    rev_clusters.push_back( new_clusters ) ;
+                }else if( counter == 1 ){ 
+                    rev_clusters_1.push_back( new_clusters) ;
+                }else if(counter == 2){ 
+                    rev_clusters_2.push_back( new_clusters ) ;
+                }else if( counter == 3 ){ 
+                    rev_clusters_3.push_back( new_clusters) ;
+                }
             }else if(!data1.reverse){
-                // cout << "inserting forward data here samflag " << data1.samFlag << endl ; 
-                for_clusters.push_back( new_clusters ) ; 
-                //potential.push_back( new_clusters ) ;
+                if(counter == 0){ 
+                    for_clusters.push_back( new_clusters ) ;
+                }else if( counter == 1 ){ 
+                    for_clusters_1.push_back( new_clusters) ;
+                }else if(counter == 2){ 
+                    for_clusters_2.push_back( new_clusters ) ;
+                }else if( counter == 3 ){ 
+                    for_clusters_3.push_back( new_clusters) ;
+                }
             }
             new_clusters.clear() ; 
             //cout << data1.samFlag << " this is the random spot to be here right now " << endl ; 
@@ -207,6 +237,24 @@ int main(int argc, char * argv[]){
     printCluster(for_clusters, outFile) ; 
     printCluster(rev_clusters, outFile) ; 
     search_potential_breaks(for_clusters, rev_clusters, cluster_size, goodFile, summary) ; 
+
+    merging_clusters(for_clusters_1) ;
+    merging_clusters(rev_clusters_1) ; 
+    printCluster(for_clusters_1, outFile) ; 
+    printCluster(rev_clusters_1, outFile) ; 
+    search_potential_breaks(for_clusters_1, rev_clusters_1, cluster_size, goodFile, summary) ; 
+
+    merging_clusters(for_clusters_2) ;
+    merging_clusters(rev_clusters_2) ; 
+    printCluster(for_clusters_2, outFile) ; 
+    printCluster(rev_clusters_2, outFile) ; 
+    search_potential_breaks(for_clusters_2, rev_clusters_2, cluster_size, goodFile, summary) ; 
+
+    merging_clusters(for_clusters_3) ;
+    merging_clusters(rev_clusters_3) ; 
+    printCluster(for_clusters_3, outFile) ; 
+    printCluster(rev_clusters_3, outFile) ; 
+    search_potential_breaks(for_clusters_3, rev_clusters_3, cluster_size, goodFile, summary) ; 
 
            
 //There are some files that are already separated it in a smaller scale
@@ -248,35 +296,6 @@ void algorith_search(vector< vector<location> > &current, location data, bool in
         }
     }
 }
-//compare and intersect two vectors into vector c3 
-//fix intersection sort here man. 
-vector<location> intersection(vector<location> &c1 , vector<location> &c2){
-    vector<location> c3 ; //this will be returned ; 
-    sort(c1.begin(), c1.end(), sortByLocation) ;
-    sort(c2.begin(), c2.end(), sortByLocation) ;
-    //intersection begins here by iterating from c1 and c2 to the end by position1
-    set_intersection(c1.begin(), c1.end(), 
-                    c2.begin(), c2.end(), 
-                    back_inserter(c3), compByLocation) ; 
-    sort(c3.begin(), c3.end(), sortByLocation) ; 
-    return c3 ; 
-}
-
-//compare and take out the unique elements of both vectors into vector c3
-vector<location> unique_difference(vector<location> &c1, vector<location> &c2){
-    vector<location> c3 ; 
-    // cout <<"in here in unique difference" << endl ;
-    sort(c1.begin(), c1.end(), sortByLocation) ;
-    sort(c2.begin(), c2.end(), sortByLocation) ;
-    //same idea as intersection but instead of looking for matching positions1, I look for non matching ones. 
-    set_symmetric_difference(c1.begin(), c1.end(), 
-                            c2.begin(), c2.end(), 
-                            back_inserter(c3), compByLocation1) ; 
-    sort(c3.begin(), c3.end(), sortByLocation) ; 
-    return c3 ; 
-    
-}
-
 
 vector<location> duplicate ( vector<location> &v1 , vector<location> &v2 ) { 
     map<location,int> read_counts ; 
@@ -323,7 +342,6 @@ void merging_clusters( vector< vector<location> > &current) {
     for ( int i = current.size() ; i > 0 ; i -- ) { 
         for ( int i2 = i - 1 ; i2 > -1 ; i2 -- ) { 
             vector<location> dup = duplicate( current[i], current[i2] ) ;
-            cout << " beforehand segfault? " << endl ;  
             if ( dup.size() > 0 ) { 
                 // only go through here if it's not at the start of the program. '
                 if(delete_counter != 0 && i != current.size()){
@@ -368,7 +386,7 @@ void search_potential_breaks(vector< vector<location> > &forward , vector< vecto
                 if (reverse[r].size() > 4) { 
 
                     if( ( abs( f_strand_1 - r_strand_1 ) < 30000 && abs( f_strand_1 - r_strand_1 ) < 30000  ) \
-                    || ( abs( f_strand_2 - r_strand_2 ) < 30000 && abs( f_strand_2 - r_strand_2) < 30000 ) ) {
+                    && ( abs( f_strand_2 - r_strand_2 ) < 30000 && abs( f_strand_2 - r_strand_2) < 30000 ) ) {
                         
                         //that might not work in general.
                         //this in theory should print out forward and reverse clusters together to show they are a good match or not
@@ -384,8 +402,8 @@ void search_potential_breaks(vector< vector<location> > &forward , vector< vecto
                                 if(j == 0 || j == forward[f].size()-1){ //for summary file 
                                     summary << r << "\t";
                                     summary << reverse[r][j].ID << "\t" << reverse[r][j].samFlag << "\t" << reverse[r][j].chrom_pos << "\t" << reverse[r][j].map_quality << "\t" << reverse[r][j].position1 << "\t" << reverse[r][j].position2 << '\n'; 
+                                    summary << endl ; 
                                 }
-                                summary << endl ; // making sure they are separated by an empty line.
                                 out << r << "\t";
                                 out << reverse[r][j].ID << "\t" << reverse[r][j].samFlag << "\t" << reverse[r][j].chrom_pos << "\t" << reverse[r][j].map_quality << "\t" << reverse[r][j].position1 << "\t" << reverse[r][j].position2 << '\n'; 
                             }
@@ -409,32 +427,5 @@ bool sortByLocation(const location &lhs , const location &rhs){
     }else if(lhs.position1 == rhs.position1){
         return false ;
     }
-    // if(lhs.position2 < rhs.position2){
-    //     return lhs.position2 < rhs.position2 ; 
-    // }else{
-    //     return lhs.position1 < rhs.position1 ;
-    // } 
-}
-//for intersection
-bool compByLocation(const location &lhs , const location &rhs){
-    if(lhs.position1 == rhs.position1 && lhs.position2 == rhs.position2){
-        // cout << "in true for compbylocation" << endl ; 
-        return true ; 
-    }else { 
-        // cout << "in false for compybylocation" << endl ; 
-        return false ; 
-    }
-}
-//for unique difference
-bool compByLocation1(const location &lhs , const location &rhs){
-    if(lhs.position1 == rhs.position1){
-        return false  ; 
-    }else { 
-        return true ; 
-    }
 }
 
-//comparing the first positions and last positions 
-bool compFirst(vector<location> c1, vector<location> c2) { 
-
-}
