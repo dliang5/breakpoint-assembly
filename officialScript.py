2cf5ee90ea282b793e006d1ad33a2132fa9953b8
TheFoodGuy <davidxliang@gmail.com>
2017-04-28 12:32:51 -0700

    finished fixing the script files to run RAL files now, just gotta make sure the program now checks for nonKnownInversions

--- officialScript.py
+++ officialScript.py
@@ -11,15 +11,37 @@ import os, subprocess as sp, sys
 # Ex. Name, name1, originalRegion, SRRname, chromosomes
 
 #however, we are going to check RAL files first
+with open ("RAL-README", 'w') as f: 
+    f.write("this is here to create the RAL file and store all of the corresponding information in there")
+    
+inputFile = "RAL-README" 
+writeFile = open(inputFile, 'a') 
 
 with open("TableS1_individuals.csv", 'r') as poolFile: 
     sp.Popen("mkdir RAL", shell=True, executable='/bin/bash')
     for line in poolFile: 
         content = [x.strip() for x in line.split(",")]
-        if content[:3] != 'RAL': 
+        setName = content[0] 
+        if setName[:3] != 'RAL': 
+            # no need for other yet
             continue 
         else: 
-            SRRname = content[3] 
-            path = "qsub running3.sh" + SRRname + " >> RAL" 
-            sp.Popen(path, shell=True, executable='/bin/bash')
-            
+            checkTwice = 'SR'
+            srrTwice = content[4]  
+            if ( srrTwice[:2] == checkTwice): 
+                SRRname = content[3]
+                SRRname1 = content[4] 
+                print (setName,content[3], content[4])
+                path = "qsub running3.sh " + setName + " " + SRRname + " " + SRRname1
+                sp.Popen(path, shell=True, executable= '/bin/bash')
+                writeFile.write("{0} {1} {2}\n".format(setName, SRRname, SRRname1))
+                    
+            else: 
+                SRRname = content[3] 
+                path = "qsub running3.sh " + setName+ " " +SRRname  + " " + 0
+                # goes to the first one
+                print (setName, content[3])
+                sp.Popen(path, shell=True, executable='/bin/bash')
+                writeFile.write("{0} {1}\n".format(setName, SRRname)) 
+
+writeFile.close()
--- running3.sh
+++ running3.sh
@@ -2,37 +2,65 @@
 #!/bin/bash 
 
 # this should do all of the processes from downloading the SRR file to running the assembly.
-srrName=$1
+srrName=$2
+srrName1=$3 # this is optional variable
+setName=$1
+setFile="${setName}.bam"
+name1Fastq1="${srrName1}_1.fastq.gz" 
+name1Fastq2="${srrName1}_2.fastq.gz" 
+name1BamName="${srrName1}.bam"
+
 fastq1="${srrName}_1.fastq.gz" 
 fastq2="${srrName}_2.fastq.gz"
 bamName="${srrName}.bam" 
-sortedBam="${srrName}_sorted.bam"
-samName="${srrName}.sam"
+
+sortedBam="${setName}_sorted.bam"
+samName="${setName}.sam"
+
+
+resultFile="${setName}-result"
+# breakPointFile="${setName}-breakpoints"
+
 fastq-dump --gzip --split-files $srrName
 bwa mem ../../DGRP/1_SRR_file/chrom_ref $fastq1 $fastq2 | samtools view -Sb - > $bamName
-samtools view -h -o $samName $bamName
-samtools sort $bamName -o $sortedBam 
-samtools index $sortedBam 
 
-#done downloading and mapping everything here now 
-resultFile="${srrName}-result"
-breakPointFile="${srrName}-breakpoints"
-idBreakPoints="id${breakPointsFile}.txt" #getting the ID names 
-BreakPointReads="${srrName}_reads.txt" #obtained all the reads from fastq 
-./reading $samName $resultFile
-python search_transposeable.py $srrName 
-if [-s $breakPointFile ]
+# checking for a second SRRname file here to append
+if [ $srrName1 -eq 0 ]
 then 
-    echo "${srrName} has nothing in it" >> badBreakPoints
-    #badBreakPoints = nothing of potential breakpoints 
-# else 
-#     asFile="${srrName}_1.out" #file produced from fastq2fa_qual.pl in fasta format 
-#     qualAsFile="${srrName}_1.out.qual" #file produced from fastq2fa_qual.pl in qual mode 
-#     cut -f2 < $breakPointsFile > $idBreakPoints
-#     gzip -dc $fastq1 | perl parse_reads.pl $idBreakPoints > $BreakPointReads
-#     gzip -dc $fastq2 | perl parse_reads.pl $idBreakPoints >> $BreakPointReads
-#     perl fastq2fa_qual.pl < $BreakPointReads > $asFile 2> $qualAsFile 
-#     # phrap -vector_bound 0 -forcelevel 10 $BreakPointReads
-#     echo "${srrName} is done"
+    samtools view -h -o $samName $bamName
+    samtools sort $bamName -o $sortedBam 
+    samtools index $sortedBam 
+
+    #done downloading and mapping everything here now 
+    # idBreakPoints="id${breakPointsFile}.txt" #getting the ID names 
+    # BreakPointReads="${setName}_reads.txt" #obtained all the reads from fastq 
+    ./reading $samName $resultFile
+    python search_transposeable.py $setName  
+else
+    # second SRRname is found 
+    fastq-dump --gzip --split-files $srrName1 
+    bwa mem ../../DGRP/1_SRR_file/chrom_ref $name1Fastq1 $name1Fastq2 | samtools view -Sb - > $name1BamName
+    samtools merge $setFile $bamName $name1BamName
+    samtools view -h -o $samName $setFile 
+    samtools sort $setFile -o $sortedBam
+    samtools index $sortedBam 
+    ./reading $samName $resultFile 
+    python search_transposeable.py $setName
 fi 
+
+
+# if [-s $breakPointFile ]
+# then 
+#     echo "${srrName} has nothing in it" >> badBreakPoints
+#     #badBreakPoints = nothing of potential breakpoints 
+# # else 
+# #     asFile="${srrName}_1.out" #file produced from fastq2fa_qual.pl in fasta format 
+# #     qualAsFile="${srrName}_1.out.qual" #file produced from fastq2fa_qual.pl in qual mode 
+# #     cut -f2 < $breakPointsFile > $idBreakPoints
+# #     gzip -dc $fastq1 | perl parse_reads.pl $idBreakPoints > $BreakPointReads
+# #     gzip -dc $fastq2 | perl parse_reads.pl $idBreakPoints >> $BreakPointReads
+# #     perl fastq2fa_qual.pl < $BreakPointReads > $asFile 2> $qualAsFile 
+# #     # phrap -vector_bound 0 -forcelevel 10 $BreakPointReads
+# #     echo "${srrName} is done"
+# fi 
     
