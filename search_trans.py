d06dd94f0c00fbfcb638ea72282b3fc09a3e7145
David Liang <davidxliang@gmail.com>
2017-11-17 16:09:01 -0800

    updating trans_search.py to fix the formatting of the output file

--- search_trans.py
+++ search_trans.py
@@ -95,6 +95,7 @@ with open("TE_sites.txt", "r") as f:
 break_store = [] # this will hold a list of classes 
 check_sum = 0 
 summaryResults = "summary_"+name[1].lstrip("'").rstrip("']")+"-result" 
+# summary_857-result 
 with open(summaryResults, "r") as f: 
     for line in f: 
         # if there's an empty line as seen in the summary file, then it breaks into another list.
@@ -129,7 +130,6 @@ for index, cur_list in enumerate(break_store): # getting each individual list
         in_transrange = False 
         for te in te_objects:
             if cur_breakpoint.getchrom_ref() == te.getchrom_ref(): # making sure the location is the same
-            # somehow set_5_8 goes through. 
                 if te.getposition1() <= cur_breakpoint.getlowposition1() <= te.getposition2() or te.getposition1() <= cur_breakpoint.gethighposition1() <= te.getposition2()\
                  or te.getposition1() <= cur_breakpoint.getlowposition2() <= te.getposition2() or te.getposition1() <= cur_breakpoint.gethighposition2() <= te.getposition2():
                     in_transrange = True
@@ -140,32 +140,30 @@ for index, cur_list in enumerate(break_store): # getting each individual list
     if in_transrange == False:
         notTE.append(cur_list)
 
-# # TODO : what is this one tho? I can't recall?
-# summaryBreakpoints = "summary_"+name[1].lstrip("'").rstrip("']")+"-breakpoints"
-# with open(summaryBreakpoints, 'w') as f: 
-#     for breakpoints in notTE: 
-#         for index in breakpoints: 
-#             f.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n".format( index.getsamflag(), index.getchrom_ref(), index.getlowposition1(), index.getlowposition2(), index.gethighposition1(), index.gethighposition2(), index.getKnown() ))
-
+summarySam = "summary_"+name[1].lstrip("'").rstrip("']")+"-breakpoints"
 # printing a new summary file here
-with open(  ) # TODO HERE MAN 
-
-
-
+with open(summarySam, 'w' ) as wf: 
+    for breakpoints in notTE:
+        for index in breakpoints:
+            wf.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n".format( index.getsamflag(), index.getchrom_ref(), index.getlowposition1(), 
+                                    index.getlowposition2(), index.gethighposition1(), index.gethighposition2(), index.getSize() ))
+        wf.write("\n")
 goodReadFile = "good_"+name[1].lstrip("'").rstrip("']")+"-result" # ex. good_857-result
-detailedBreakpoints = name[1].lstrip("'").rstrip("']")+"-breakpoints" # 857-result is the most important file in here
+detailedBreakpoints = "good_"+name[1].lstrip("'").rstrip("']")+"-breakpoints" # 857-result is the most important file in here
 writeDetailedBreakpoints = open(detailedBreakpoints, 'w')
 
 # this is opening good_<SRR>-results which is has the full info on the clusters
+
 with open(goodReadFile) as f:
     for line in f:
         content = [x.strip() for x in line.split("\t")]
         if len(content) != 7: # to avoid getting an error for index out of range due to the empty line separating them? 
-            continue 
-        for i in newBreakStore: 
+            writeDetailedBreakpoints.write("\n")
+            continue
+
+        for i in notTE: 
             for j in i: 
                 if content[0] == str( j.getindex() ): 
                     writeDetailedBreakpoints.write(line)
-                    break  
-                    
+                    break                      
                 
