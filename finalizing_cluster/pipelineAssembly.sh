f11c846bfc7291a23e1996a23ef5645031a9d069
David Liang <dliang5@ucsc.edu>
2018-04-02 10:12:00 -0700

    finished the pipeline, but I would need to double check on them

--- finalizing_cluster/pipelineAssembly.sh
+++ finalizing_cluster/pipelineAssembly.sh
@@ -6,3 +6,22 @@
 
 # this calls the other five files to read and parse the files for assembly 
 # after this then we are done for now 
+
+
+# what do I have to work on this
+
+#1. call parse_id.py to obtain the id from the bam file
+#2. call reads.py to gather the fastq files
+#3. then call fastq2fa_qual.pl to obtain the fasta and qual files 
+#4. then and finally call the assembly of both of them and you're done
+
+bam=$1
+chrom=$2
+firstpos=$3 
+secondpos=$4
+#1 and #2 here 
+samtools view $bam $chrom":"$firstpos"-"$secondpos | python2.7 parse_id.py $chrom $firstpos $secondpos > $1".id" 
+#3 
+perl fastq2fa_qual.pl < $1".id" > $1".fasta" 2> $1".fasta.qual"
+#4
+phrap -vector_bound 0 -forcelevel 10 $1".fasta"
